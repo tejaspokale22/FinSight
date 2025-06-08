9ad69806891eca653c8074dd3148a6ec02c8faaf
tejaspokale22 <tejaspokale22@gmail.com>
2025-06-08 21:24:56 +0530

    Fix prisma issue

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,7 +7,9 @@ const globalForPrisma = global as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["query"],
-  }); 
+    log: process.env.NODE_ENV === "development" ? ["query"] : [],
+  });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
